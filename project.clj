6649d6d654ba5e74075de42756022a780d051cf5
OHTA Shogo <athos0220@gmail.com>
2017-01-16 00:43:50 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject spectrace "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A utility library to trace and enumerate specs involved in a given spec error"
+  :url "https://github.com/athos/spectrace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]])
