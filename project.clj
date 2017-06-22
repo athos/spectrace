9188d79a4fd19b6b33851f38cee87807b465ab50
OHTA Shogo <athos0220@gmail.com>
2017-06-22 14:16:40 +0900

    Remove spec.alpha from dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,7 @@
   :test-paths ["test/cljc"]
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/clojurescript "1.9.562"]
-                 [org.clojure/spec.alpha "0.1.123"]]
+                 [org.clojure/clojurescript "1.9.562"]]
   :plugins [[lein-eftest "0.3.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha1"]]}}
   :aliases {"test" ["eftest"]})
